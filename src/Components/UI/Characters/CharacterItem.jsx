dd9c8fb32bfb0c7b93778bca35d6c2f4ff079ff3
APPLE <apple@APPLEs-MacBook-Pro.local>
2021-05-29 19:51:40 +0530

    UI

--- src/Components/UI/Characters/CharacterItem.jsx
+++ src/Components/UI/Characters/CharacterItem.jsx
@@ -7,7 +7,7 @@ const CharacterItem = ({ item }) => {
         <div className='card'>
       <div className='card-inner'>
         <div className='card-front'>
-          <img src={item.img} alt='' />
+          <img src={item.img} alt=''/>
         </div>
         <div className='card-back'>
           <h1>{item.name}</h1>
